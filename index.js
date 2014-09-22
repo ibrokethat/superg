c615ae6118749a11cd6919c6e37475b475f49e41
Simon Jefford <si@ibrokethat.com>
2014-09-22 15:34:08 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+var path = require('path');
+var co = require('co');
+var CONFIG = require('config');
+var moment = require('moment-timezone');
+
+// globally set the timezone to UTC
+process.env.TZ = 'UTC';
+
+// globally set the locale of moment
+moment.locale(CONFIG.date.lang);
+// set start day of the week
+moment.localeData(CONFIG.date.lang)._week.dow = CONFIG.date.start_of_week;
+
+//  load the src files
+var src = require('require-all')(path.join(process.cwd(), 'src'));
+
+module.exports.app = {};
+
+co(function* () {
+    // start the server
+    module.exports.app = yield require('superposition')(src);
+})();
---
+++ src/v0.0.0/components/chart/render.js
@@ -0,0 +1,3 @@
+module.exports = function render (root) {
+
+}
